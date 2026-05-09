8c13dc131edc2c70c11985467c66fc8b726d7eef
stdiomov-creator <stdiomov@gmail.com>
2026-05-09 17:28:05 +0300

    update site

---
+++ push.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+GIT_EXEC_PATH="/Applications/GitHub Desktop.app/Contents/Resources/app/git/libexec/git-core" \
+/Applications/GitHub\ Desktop.app/Contents/Resources/app/git/bin/git \
+  -C ~/morphe-site push origin main
